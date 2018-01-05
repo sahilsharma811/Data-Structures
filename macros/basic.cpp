67c74ac6c787f35fcd1a78a56927065b54460a01
sahilsharma811 <sahil8sharma8@gmail.com>
2018-01-06 02:08:46 +0530

    Enhacements

--- macros/basic.cpp
+++ macros/basic.cpp
@@ -1,19 +1,23 @@
 #include <iostream>
-#define in1 cin>>
-
+// Defining Macros 
+#define inp cin>>
 #define inarr(a,n) for(int i=0;i<n;i++) cin>>a[i]
-
-#define p_arr(a,n) for(int i=0; i<n; i++) cout<<a[i]<<" "
-
+#define p_arr(a,n) for(typeof(a[0]) i=0; i<n; i++) cout<<a[i]<<" "
 #define For(start,end) for(int i=start; i<end; i++)
+#define MAX(a,b) (a>b ? a:b)
+// Defining constant
+#define N 100  
+#define M 200
+// Defining Typedef
+typedef long long ll;
+typedef int my_int;
 
 using namespace std;
-
 int main() {
 	// your code goes here
-	int n,arr[10];
-	
-	in1 n;
+	my_int n,arr[10];
+	ll var = 1002010020;
+	inp n;
 	inarr(arr,n);
 	
 	p_arr(arr,n);
@@ -22,5 +26,9 @@ int main() {
 	For(1,4)
 	cout<<arr[i]<<' ';
 	
+	cout<<endl;
+	
+	cout<<"Maximum is "<<MAX(N,M);
+	cout<<endl<<"Long Long Double Varialble values is "<<var;
 	return 0;
 }
